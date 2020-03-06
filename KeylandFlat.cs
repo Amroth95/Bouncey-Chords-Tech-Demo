999c50a9aff028b12c5a26f2b32b0ab43c31ccb9
Amroth95 <sproatharry119@gmail.com>
2020-03-06 16:58:58 +1300

    Bouncey Chords Demo V1.0
    UPDATES
    - Code functions for both flat and sharp keys when collision with the player is detected.
    
    KNOWN BUGS/ISSUES
    - keys on the upper octive play the wrong notes
    
    NEED TO DO
    - Create full key board.

---
+++ KeylandFlat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeylandFlat : MonoBehaviour
+{
+    AudioSource Audiosource;
+    Animation Animationsource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      Audiosource = GetComponent<AudioSource>();
+      Animationsource = GetComponent<Animation>();
+      gameObject.GetComponent<Renderer>().material.color = Color.white;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Audiosource.Play();
+        Animationsource.Play();
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        StartCoroutine(ChangeColorBack());
+    }
+
+    IEnumerator ChangeColorBack()
+    {
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ KeylandSharp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeylandSharp : MonoBehaviour
+{
+    AudioSource Audiosource;
+    Animation Animationsource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      Audiosource = GetComponent<AudioSource>();
+      Animationsource = GetComponent<Animation>();
+      gameObject.GetComponent<Renderer>().material.color = Color.black;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Audiosource.Play();
+        Animationsource.Play();
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        StartCoroutine(ChangeColorBack());
+    }
+
+    IEnumerator ChangeColorBack()
+    {
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<Renderer>().material.color = Color.black;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
