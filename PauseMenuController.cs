d35d8b9ce360d49568d2a828c037c9e36ece6472
Amroth95 <sproatharry119@gmail.com>
2020-03-11 16:22:46 +1300

    Bouncey Chords Demo V1.2
    UPDATES
    - Prototype UI created for game. These are the main menu and pause menu.
    - Visual changes being added into the prototype level, starting with glowing keys.
    
    KNOWN BUGS/ISSUES
    - keys on the upper octive play the wrong notes.
    
    NEED TO DO
    - Create full key board.
    - Complete menu UI.

---
+++ PauseMenuController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    public GameObject PauseMenu;
+    public bool IsPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused)
+            {
+                IsPaused = false;
+                PauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+            } else
+            {
+                IsPaused = true;
+                PauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+
+    public void ResumeGame()
+    {
+        if(IsPaused)
+        {
+            IsPaused = false;
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        } else
+        {
+            IsPaused = true;
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void RestartGame()
+    {
+      SceneManager.LoadScene("Bouncey Chords Proto");
+      Time.timeScale = 1f;
+    }
+
+    public void OptionsMenu()
+    {
+        
+    }
+
+    public void ReturnToMainMenu()
+    {
+      Time.timeScale = 1f;  
+      SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ExitGame()
+    {
+      Debug.Log("QUIT!");
+      Application.Quit();
+    }
+}
+
